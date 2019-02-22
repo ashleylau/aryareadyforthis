1fe400bb6a142caf404a858f452e728e448ac52a
ashleylau <ablau@stanford.edu>
2019-02-22 12:48:43 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@ int checkSwitches(void);
 
 //Global variables
 #define LEFT_MOTOR_ENABLE   3
-#define LEFT_MOTOR_DIR      4
+#define LEFT_MOTOR_DIR1     4
+#define LEFT_MOTOR_DIR2     5
 #define RIGHT_MOTOR_ENABLE  22
-#define RIGHT_MOTOR_DIR     23
+#define RIGHT_MOTOR_DIR1     23
+#define RIGHT_MOTOR_DIR2     21
 #define LIMIT_FRONT         14
 #define LIMIT_BACK          15
 
-int left_dir = HIGH;
-int right_dir = HIGH;
+int left_dir1 = HIGH;
+int right_dir1 = HIGH;
+int left_dir2 = LOW;
+int right_dir2 = LOW;
 int speed = 127;
 
 void setup() {
@@ -20,34 +24,45 @@ void setup() {
   Serial.begin(9600);
   while(!Serial);
   pinMode(LEFT_MOTOR_ENABLE, OUTPUT);
-  pinMode(LEFT_MOTOR_DIR, OUTPUT);
+  pinMode(LEFT_MOTOR_DIR1, OUTPUT);
+  pinMode(LEFT_MOTOR_DIR2, OUTPUT);
   pinMode(RIGHT_MOTOR_ENABLE, OUTPUT);
-  pinMode(RIGHT_MOTOR_DIR, OUTPUT);
+  pinMode(RIGHT_MOTOR_DIR1, OUTPUT);
+  pinMode(RIGHT_MOTOR_DIR2, OUTPUT);
   pinMode(LIMIT_FRONT, INPUT_PULLUP);
   pinMode(LIMIT_BACK, INPUT_PULLUP);
-  analogWrite(LEFT_MOTOR_ENABLE, speed);
-  analogWrite(RIGHT_MOTOR_ENABLE, speed);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  analogWrite(LEFT_MOTOR_ENABLE, speed);
+  analogWrite(RIGHT_MOTOR_ENABLE, speed);
+  
   int sideHit = checkSwitches();
   if (sideHit == 0){
     //do nothing
   }
   else if (sideHit == 1){
     //Front limit switch hit; set both motors to low
-    left_dir = LOW;
-    right_dir = LOW;
-    digitalWrite(LEFT_MOTOR_DIR, left_dir);
-    digitalWrite(RIGHT_MOTOR_DIR, right_dir);
+    left_dir1 = LOW;
+    right_dir1 = LOW;
+    left_dir2 = HIGH;
+    right_dir2 = HIGH;
+    digitalWrite(LEFT_MOTOR_DIR1, left_dir1);
+    digitalWrite(LEFT_MOTOR_DIR2, left_dir2);
+    digitalWrite(RIGHT_MOTOR_DIR1, right_dir1);
+    digitalWrite(RIGHT_MOTOR_DIR2, right_dir2);
   }
   else if (sideHit == 2){
     //Back limit switch hit; set both motors to high
-    left_dir = HIGH;
-    right_dir = HIGH;
-    digitalWrite(LEFT_MOTOR_DIR, left_dir);
-    digitalWrite(RIGHT_MOTOR_DIR, right_dir);
+    left_dir1 = HIGH;
+    right_dir1 = HIGH;
+    left_dir2 = LOW;
+    right_dir2 = LOW;
+    digitalWrite(LEFT_MOTOR_DIR1, left_dir1);
+    digitalWrite(LEFT_MOTOR_DIR2, left_dir2);
+    digitalWrite(RIGHT_MOTOR_DIR1, right_dir1);
+    digitalWrite(RIGHT_MOTOR_DIR2, right_dir2);
   }
 }
 
