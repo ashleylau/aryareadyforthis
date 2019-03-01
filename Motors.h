8a2c46835d5d93d053bab149178e64be04899d96
ashleylau <ablau@stanford.edu>
2019-02-28 20:42:35 -0800

    just added a comment denoting naming

--- Motors.h
+++ Motors.h
@@ -10,6 +10,14 @@
 #define FRONT_BACK_DIR2         19  //Not PWM
 #define FRONT_BACK_ENABLE       20  //PWM
 
+/* ---- Note about naming: -----
+For reference: "Front" should be pointing towards the arena wall
+Left and Right refer to the left and right motors (not the direction the robot is moving)
+    To drive front/back: Drive the left and right motors
+Front and Back refer to the front and back motors (not the direction the robot is moving)
+    To drive left/right: Drive the front and back motors
+**/
+
 class MotorsClass{
     public:
         MotorsClass();
