3728166221bcb07c1020bd324355ef8a55a47f17
ashleylau <ablau@stanford.edu>
2019-03-14 23:42:12 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ void checkOff(void);
 void destroyWall(void);
 void towersFirst(void);
 void reload(void);
+void testUltrasonic(void);
 long getDistance(void);
+void shutItDown(void);
 
-//NEED TO MAKE SURE WE STOP EVERYTHING AFTER 2:10
 
 //Global variables  
 #define LIMIT_LEFT          9
@@ -28,8 +29,8 @@ long duration, inches;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  while(!Serial);
+  //Serial.begin(9600);
+  //while(!Serial);
 
   pinMode(LIMIT_FRONT, INPUT_PULLUP);
   pinMode(LIMIT_BACK, INPUT_PULLUP);
@@ -39,20 +40,16 @@ void setup() {
   Serial.println("hacked the mainframe");
 
   //Set servo to correct position
-  launcher.turnOffServo();
-  launcher.powerServo();
   Serial.println("Initial clear from twitch");
   launcher.shootBall();
   launcher.clearSwitch();
-  
 
   //Run the check-off Casterly Rock shooting process
   checkOff();
   //Detect which mode we want to use
   //If switched to destroy the wall first
-  destroyWall();
   //If switched to destroying more towers before the wall
-  //towersFirst();
+  towersFirst();
 }
 
 void loop() {
@@ -69,131 +66,21 @@ void checkOff() {
   }
   reload();
   //Starts launching the balls at Casterly Rock
-  //launcher.startFlywheel();
+  launcher.startFlywheel();
+  motors.rideFrontWallRight();
+  delay(400);
+  motors.stopMoving();
   Serial.println("Wait two seconds to shoot balls");
   delay(2000);
-  int counter = 0;
+  int ballCounter = 0;
   
-  while (counter < 2){
+  while (ballCounter < 1){
     Serial.println("Next ball");
     launcher.shootBall();
     launcher.clearSwitch();
-    counter++;
+    ballCounter++;
     delay(2000);
   }
-  
-}
-
-void destroyWall(void){
-  int halfWallDist = 46;
-  //Knock down bricks 4,5,6,7,8
-  for(int i = 0; i < 5; i++){
-    int distance = getDistance();
-    motors.rideFrontWallRight();
-    while(distance > (halfWallDist - (6*i))){
-      //wait
-      distance = getDistance();
-      Serial.println(distance);
-    }
-    Serial.println(distance);
-    motors.stopMoving();
-    delay(2000);
-    launcher.shootBall();
-    launcher.clearSwitch();
-  }
-  //Start moving back to reload
-  //launcher.stopFlywheel();
-  Serial.println("out of tower destroying while loop");
-  motors.rideFrontWallRight();
-  
-  int distance = getDistance();
-  while(distance > 5){
-    //wait
-    distance = getDistance();
-  }
-  
-  motors.moveBackward();
-  while(!digitalRead(LIMIT_BACK)){
-    //wait
-  }
-  reload();
-  //Shoot kings landing
-  motors.rideFrontWallRight();
-  
-  int distance = getDistance();
-  while(distance > 46){
-    //wait
-    distance = getDistance();
-  }
-  
-  //launcher.startFlywheel();
-  motors.stopMoving();
-  launcher.shootBall();
-  launcher.clearSwitch();
-  //Knock over brick 9
-  motors.rideFrontWallRight();
-  int distance = getDistance();
-  while(distance > 22){
-    //wait
-    distance = getDistance();
-  }
-  motors.stopMoving();
-  launcher.shootBall();
-  launcher.clearSwitch();
-  //Knock over brick 10, hit dragonstone
-  motors.rideFrontWallRight();
-  
-  int distance = getDistance();
-  while(distance> 18){
-    //wait
-    distance = getDistance();
-  }
-  
-  motors.stopMoving();
-  launcher.shootBall();
-  launcher.clearSwitch();
-  launcher.shootBall();
-  launcher.clearSwitch();
-  //Destroy rest of wall
-  motors.rideFrontWallLeft();
-
-  int distance = getDistance();
-  while(distance < 52){
-    //wait
-    distance = getDistance();
-  }
-
-  motors.stopMoving();
-  launcher.shootBall();
-  launcher.clearSwitch();
-  motors.rideFrontWallLeft();
-  
-  int distance = getDistance();
-  while(distance < 58){
-    //wait
-    distance = getDistance();
-  }
-  
-  motors.stopMoving();
-  launcher.shootBall();
-  launcher.clearSwitch();
-  //Move back to reload
-  launcher.stopFlywheel();
-  motors.moveBackward();
-  while(!digitalRead(LIMIT_BACK)){
-    //wait
-  }
-  reload();
-  //Start arbitrarily firing at dragon??
-  //launcher.startFlywheel();
-  int counter = 0;
-  //If we add sensor to check for dragon, add that here?
-  while (counter < 6){
-    delay(1000);
-    launcher.shootBall();
-    launcher.clearSwitch();
-  }
-  launcher.stopFlywheel();
 }
 
 //Focuses on destroying all of the towers before the wall
@@ -206,26 +93,39 @@ void towersFirst(void){
     //wait
     distance = getDistance();
   }
+  motors.stopMoving();
   //Knock over wall
-  //launcher.startFlywheel();
+  launcher.startFlywheel();
   launcher.shootBall();
   launcher.clearSwitch();
-  //Hit King's Landing
   launcher.shootBall();
   launcher.clearSwitch();
+  launcher.shootBall();
+  launcher.clearSwitch();
+  delay(500);
   
   //Hit Dragonstone
   motors.rideFrontWallRight();
-  int distance = getDistance();
+  distance = getDistance();
   while(distance > 18){
     //wait
     distance = getDistance();
   }
+  motors.stopMoving();
   //Knock over wall
   launcher.shootBall();
   launcher.clearSwitch();
+  //Shoot Dragonstone
   launcher.shootBall();
   launcher.clearSwitch();
+  launcher.shootBall();
+  launcher.clearSwitch();
+  launcher.shootBall();
+  launcher.clearSwitch();
+  launcher.shootBall();
+  launcher.clearSwitch();
+
+  delay(500);
 
   //Move backward and reload
   launcher.stopFlywheel();
@@ -235,51 +135,65 @@ void towersFirst(void){
   }
   reload();
 
-  //Destroy wall
+  launcher.startFlywheel();
   motors.rideFrontWallRight();
-  //launcher.startFlywheel();
-  int distance = getDistance();
-  while(distance > 64){
-    //wait
-    distance = getDistance();
-  }
+  delay(375);
   motors.stopMoving();
-  launcher.shootBall();
-  launcher.clearSwitch();
-  int initBlock = 51;
-  //Knock down most of the wall
-  for(int i = 0; i < 5; i++){
-    motors.rideFrontWallRight();
-    int distance = getDistance();
-    while(distance > (initBlock - (i*6))){
-      //wait
-      distance = getDistance();
-    }
-    motors.stopMoving();
+  //Start arbitrarily firing at dragon??
+  int counter = 0;
+  //If we add sensor to check for dragon, add that here?
+  while (counter < 8){
+    //if(millis() > 110000) shutItDown();
+    delay(1000);
     launcher.shootBall();
     launcher.clearSwitch();
+    counter++;
   }
+  launcher.stopFlywheel();
   motors.rideFrontWallRight();
-  int distance;
-  while(distance > 5){
-    //wait
-    distance = getDistance();
-  }
+  delay(turnTime);
   motors.moveBackward();
   while(!digitalRead(LIMIT_BACK)){
     //wait
   }
   reload();
+
+  launcher.startFlywheel();
+  motors.rideFrontWallRight();
+  delay(350);
+  motors.stopMoving();
   //Start arbitrarily firing at dragon??
-  //launcher.startFlywheel();
-  int counter = 0;
+  counter = 0;
   //If we add sensor to check for dragon, add that here?
-  while (counter < 6){
+  while (counter < 8){
+    //if(millis() > 110000) shutItDown();
     delay(1000);
     launcher.shootBall();
     launcher.clearSwitch();
+    counter++;
   }
   launcher.stopFlywheel();
+  motors.rideFrontWallRight();
+  delay(turnTime);
+  motors.moveBackward();
+  while(!digitalRead(LIMIT_BACK)){
+    //wait
+  }
+  reload();
+
+  launcher.startFlywheel();
+  //Start arbitrarily firing at dragon??
+  counter = 0;
+  //If we add sensor to check for dragon, add that here?
+  while (counter < 8){
+    //if(millis() > 110000) shutItDown();
+    delay(1000);
+    launcher.shootBall();
+    launcher.clearSwitch();
+    counter++;
+  }
+  launcher.stopFlywheel();
+  motors.stopMoving();
 }
 
 
